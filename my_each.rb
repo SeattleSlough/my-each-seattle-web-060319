59aca88a2ac09cb0efe9b38003fabde464bdf72d
Michael Maggs <michaelmaggs@gmail.com>
2019-05-15 22:33:54 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,8 @@
 def my_each(array)
   i = 0 
-  if block_given == false 
-    "No block given"
-  else
-    while i < array.length 
+  while i < array.length 
       yield array[i]
       i += 1 
-    end
   end
   array
 end
