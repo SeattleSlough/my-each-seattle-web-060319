48ae1763e4a6c06a873f4e423236272f9fae111c
Michael Maggs <michaelmaggs@gmail.com>
2019-05-15 22:29:35 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
+def my_each (array)
+  i = 0 
+  if block_given == false 
+    "No block given"
+  else
+    while i < array.length 
+      yield array[i]
+      i += 1 
+      end
+    array
+  end
+      # put argument(s) here
   # code here
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
